bc6f59eaa75995d30e19f8df60ca7fb5c1600668
Vishal Myla <99010982+VishalMyla@users.noreply.github.com>
2025-03-11 16:49:10 +0530

    Stage01 of the pathways is added

---
+++ src/pages/Pathway.jsx
@@ -0,0 +1,9 @@
+import Stage1 from "../component/Pathway/stage1";
+function Pathway() {
+return (
+    <div className="w-full h-[90vh] flex justify-around items-center">
+        <Stage1/>
+    </div>
+  );
+}
+export default Pathway;
